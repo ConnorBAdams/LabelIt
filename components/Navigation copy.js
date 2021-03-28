dc09ad7b35a00c523482eed6f41a79fd9e97b889
Connor <connorbadams@gmail.com>
2021-03-28 03:40:32 -0500

    nav backup

--- components/Navigation copy.js
+++ components/Navigation copy.js
@@ -17,16 +17,7 @@ import ProfileScreen from '../screens/ProfileScreen';
 import PictureScreen from '../screens/PictureScreen';
 //#endregion
 
-/*
-This is the navigation controller
-The goal is to have the drawer navigator in here, the AppNavigator
-- This will contain all of the locations within the sidebar
-- Each of these can contain deeper navigation
-Then each page that requires a deeper navigation will have its own 
-stack navigation
-Their respective implementation can be returned from this component
-*/
-
+// This is a backup in case I break it
 
 const Tab = createBottomTabNavigator();
 export default AppNavigator = () => {
